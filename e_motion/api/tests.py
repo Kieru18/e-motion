e7ce0b983bb46ee40dedfe0a3cbbb1d62f1c6e12
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-12-04 11:32:02 +0100

    basic tests

--- e_motion/api/tests.py
+++ e_motion/api/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class BasicTest(TestCase):
+    def test_hello_world(self):
+        """Basic test description"""
+        x = 1
+        self.assertEqual(x, 1)
+
+    def test_not_equal(self):
+        """Basic test description"""
+        x = 0
+        self.assertNotEqual(x, 0)

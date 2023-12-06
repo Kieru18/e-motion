1dfcfa1e324a641858d5eb6f34c9931acac6a942
JanFidor <jan.p.fidor@gmail.com>
2023-12-06 20:45:44 +0100

    move authentication to api

--- e_motion/api/urls.py
+++ e_motion/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import UserView, home
+from .views import UserView, home, SignUpView, LoginView, TestTokenView
 
 urlpatterns = [
     path('users/', UserView.as_view()),
-    path('', home)
+    path('', home),
+    path('signup', SignUpView.as_view()),
+    path('login', LoginView.as_view()),
+    path('test_token', TestTokenView.as_view()),
 ]
         

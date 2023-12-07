3cb5662b6dbebba6ac5431c2dd559338bef9f4b5
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-12-07 01:23:03 +0100

    router added

--- e_motion/frontend/src/components/App.js
+++ e_motion/frontend/src/components/App.js
@@ -1,13 +1,30 @@
 import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
-import Dasboard from "./Dashboard";
 import SignInSide from "./LoginPage";
+import Dashboard from "./Dashboard";
+import SignUpSide from "./Register";
 
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+} from "react-router-dom";
 
 export default function App(props) {
     return (
         <div className="center">
-            <Dasboard />
+            <Router>
+                <Routes>
+                    <Route
+                        exact path="/"
+                        element={<SignInSide />}
+                    />
+                    <Route path="/login" element={<SignInSide />} />
+                    <Route path="/signup" element={<SignUpSide />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                </Routes>
+            </Router>
         </div>
     );
 }

64685c4e85b1421d29d267b87ebf8eea3e93df95
jkierucz <01169183@pw.edu.pl>
2023-12-28 21:05:20 +0100

    Updated models
    Project can now have multiple models

--- e_motion/api/models.py
+++ e_motion/api/models.py
@@ -2,18 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class LearningModel(models.Model):
-    @property
-    def model_id(self):
-        return self.id
-    
-    name = models.CharField(null=False, max_length=60)
-    hiperparams = models.CharField(null=True, max_length=600)
-    checkpoint = models.BinaryField(null=True) 
-
-    class Meta:
-        db_table = 'learning_models'
-
 
 class Project(models.Model):
     @property
@@ -22,9 +10,22 @@ class Project(models.Model):
     
     title = models.CharField(null=False, max_length=100)
     description = models.CharField(null=True, max_length=250)
-    dataset_url = models.CharField(null=False, max_length=150)
+    dataset_url = models.CharField(null=True, max_length=150)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    model = models.ForeignKey(LearningModel, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'projects'
+
+
+class LearningModel(models.Model):
+    @property
+    def model_id(self):
+        return self.id
+    
+    name = models.CharField(null=False, max_length=60)
+    hiperparams = models.CharField(null=True, max_length=600)
+    checkpoint = models.BinaryField(null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE) 
+
+    class Meta:
+        db_table = 'learning_models'

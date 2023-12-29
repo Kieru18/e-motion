1bac5a24c2dcd6caf9c9b1fcc463c0a853dfdadb
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-12-29 12:27:37 +0100

    list project url added

--- e_motion/api/urls.py
+++ e_motion/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserView, SignUpView, LoginView, TestTokenView
+from .views import UserView, SignUpView, LoginView, TestTokenView, ListProjectsView
 
 urlpatterns = [
     # path('users/', UserView.as_view()),
     path('signup', SignUpView.as_view()),
     path('login', LoginView.as_view()),
     path('test_token', TestTokenView.as_view()),
+    path('list_projects', ListProjectsView.as_view()),
 ]

0a44846e2240f352d39a6d04070ec2866d98542f
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-12-29 12:27:29 +0100

    project serializer added

--- e_motion/api/serializers.py
+++ e_motion/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Project
 from django.contrib.auth.models import User as AuthenticationUser
 
 
@@ -11,5 +11,11 @@ class UserSerializer(serializers.ModelSerializer):
 
 class RequestSerializer(serializers.ModelSerializer):
     class Meta(object):
-        model = AuthenticationUser 
+        model = AuthenticationUser
         fields = ['id', 'username', 'password', 'email']
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['title', 'description', 'dataset_url', 'user', 'model_id']

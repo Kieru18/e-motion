77d5687a6dd8de2ed7a0087f7cd094e27905b05d
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-12-29 16:46:53 +0100

    data passed as a prop

--- e_motion/frontend/src/components/ProjectsTable.js
+++ e_motion/frontend/src/components/ProjectsTable.js
@@ -6,27 +6,12 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Title from './Title';
 
-// Generate Data
-function createData(id, name, description, date, size, status) {
-  return { id, name, description, date, size, status };
-}
-
-const rows = [
-  createData(
-    0,
-    'CIFAR',
-    'object detection',
-    '07.12.2023',
-    'Small',
-    'In progress',
-  ),
-];
 
 function preventDefault(event) {
   event.preventDefault();
 }
 
-export default function ProjectsTable() {
+export default function ProjectsTable(props) {
   return (
     <React.Fragment>
       <Title>Your projects</Title>
@@ -35,19 +20,15 @@ export default function ProjectsTable() {
           <TableRow>
             <TableCell>Name</TableCell>
             <TableCell>Description</TableCell>
-            <TableCell>Date</TableCell>
-            <TableCell>Size</TableCell>
-            <TableCell>Status</TableCell>
+            <TableCell>Dataset URL</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.name}</TableCell>
+          {props.data.map((row) => (
+            <TableRow key={row.title}>
+              <TableCell>{row.title}</TableCell>
               <TableCell>{row.description}</TableCell>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.size}</TableCell>
-              <TableCell>{row.status}</TableCell>
+              <TableCell>{row.dataset_url}</TableCell>
             </TableRow>
           ))}
         </TableBody>

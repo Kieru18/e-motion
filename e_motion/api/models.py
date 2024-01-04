5df43e3a09050f6dc3d22c5df057a09556460b3b
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2024-01-04 23:34:16 +0100

    Learning_model entity updated

--- e_motion/api/models.py
+++ e_motion/api/models.py
@@ -2,12 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
 class Project(models.Model):
     @property
     def project_id(self):
         return self.id
-    
+
     title = models.CharField(null=False, max_length=100)
     description = models.CharField(null=True, max_length=250)
     dataset_url = models.CharField(null=True, max_length=150)
@@ -21,11 +20,17 @@ class LearningModel(models.Model):
     @property
     def model_id(self):
         return self.id
-    
+
     name = models.CharField(null=False, max_length=60)
-    hiperparams = models.CharField(null=True, max_length=600)
+    lr = models.FloatField(null=True)
+    weight_decay = models.FloatField(null=True)
+    epochs = models.PositiveIntegerField(null=True)
+    val_set_size = models.FloatField(null=True)
+    miou_score = models.FloatField(null=True)
+    top1_score = models.FloatField(null=True)
+    top5_score = models.FloatField(null=True)
     checkpoint = models.BinaryField(null=True)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE) 
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'learning_models'

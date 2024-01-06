bbd90cb693db01b69d657d47bf1a4a4f90eff1fc
kziarek <01169264@pw.edu.pl>
2024-01-06 21:10:57 +0100

    Set media path

--- e_motion/e_motion/settings.py
+++ e_motion/e_motion/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'uploads')
+MEDIA_URL = '/uploads/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 

a3cef663658f9da082fcb1ea7cab0bf5fe5a02f7
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2024-01-09 23:48:16 +0100

    test lists, edit, create on models and projects

--- e_motion/api/tests_views.py
+++ e_motion/api/tests_views.py
@@ -7,6 +7,7 @@ from rest_framework.test import APITestCase
 from api.models import Project, LearningModel
 from api.views import ProjectCreateView
 from django.contrib.auth.models import User
+import json
 
 
 class BasicTest(TestCase):
@@ -71,3 +72,109 @@ class ProjectCreateViewTests(APITestCase):
         }
         response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class ProjectDeleteViewTests(APITestCase):
+    def setUp(self):
+        self.test_user = User.objects.create_user(username='test', password='test')
+        self.client.force_authenticate(user=self.test_user)
+        Project.objects.create(title='title1', description='desc1', dataset_url='url1', user=self.test_user)
+        Project.objects.create(title='title2', description='desc2', dataset_url='url2', user=self.test_user)
+        self.url = "/api/delete_project"
+
+    def test_valid_request(self):
+        data = {'id': 1}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        self.assertEqual(Project.objects.count(), 1)
+        self.assertEqual(Project.objects.get().title, 'title2')
+
+    def test_no_projects(self):
+        for record in Project.objects.filter(user=self.test_user):
+            record.delete()
+        data = {'id': 1}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        self.assertEqual(Project.objects.count(), 0)
+
+
+class ProjectEditViewTests(APITestCase):
+    def setUp(self):
+        self.test_user = User.objects.create_user(username='test', password='test')
+        self.client.force_authenticate(user=self.test_user)
+        Project.objects.create(title='title1', description='desc1', dataset_url='url1', user=self.test_user)
+        Project.objects.create(title='title2', description='desc2', dataset_url='url2', user=self.test_user)
+        self.url = "/api/edit_project"
+
+    def test_valid_edit_request(self):
+        data = {"id": 1, "title": "new", "description": "desc1", "dataset_url": "url1", "user": 1}
+        expected = "new"
+
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        self.assertEqual(Project.objects.get(id=1).title, expected)
+
+    def test_invalid_request_no_id(self):
+        data = {"title": "new", "description": "desc1", "dataset_url": "url1", "user": 1}
+
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_invalid_request_incorrect_field(self):
+        data = {"id": 1, "pods": "new", "description": "desc1", "dataset_url": "url1", "user": 1}
+
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class ListProjectsViewTests(APITestCase):
+    def setUp(self):
+        self.test_user = User.objects.create_user(username='test', password='test')
+        self.client.force_authenticate(user=self.test_user)
+        Project.objects.create(title='title1', description='desc1', dataset_url='url1', user=self.test_user)
+        Project.objects.create(title='title2', description='desc2', dataset_url='url2', user=self.test_user)
+        self.url = "/api/list_projects"
+
+    def test_valid_request(self):
+        data = [
+            {"id": 1, "title": "title1", "description": "desc1", "dataset_url": "url1", "user": 1},
+            {"id": 2, "title": "title2", "description": "desc2", "dataset_url": "url2", "user": 1},
+        ]
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content), data)
+
+    def test_no_projects(self):
+        for record in Project.objects.filter(user=self.test_user):
+            record.delete()
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content), [])
+
+
+class ListModelsViewTest(APITestCase):
+    def setUp(self):
+        self.test_user = User.objects.create_user(username='test', password='test')
+        self.client.force_authenticate(user=self.test_user)
+        self.project = Project.objects.create(title='title1', description='desc1', dataset_url='url1', user=self.test_user)
+        LearningModel.objects.create(name="model1", architecture="Faster RCNN", project=self.project)
+        LearningModel.objects.create(name="model2", architecture="Faster RCNN", project=self.project)
+        self.url = f"/api/list_models/{self.project.id}/"
+
+    def test_valid_request(self):
+        data = [
+            {"id": 1, "name": "model1", "architecture": "Faster RCNN", 'miou_score': None, 'top1_score': None, 'top5_score': None},
+            {"id": 2, "name": "model2", "architecture": "Faster RCNN", 'miou_score': None, 'top1_score': None, 'top5_score': None},
+        ]
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content), data)
+
+    def test_no_models(self):
+        for record in LearningModel.objects.filter(project=self.project):
+            record.delete()
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content), [])

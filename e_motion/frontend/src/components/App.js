afd133e304339701074438fb1be8bef0e8d8a8b2
jkierucz <01169183@pw.edu.pl>
2024-01-11 03:51:17 +0100

    Added notifiaction library and support

--- e_motion/frontend/src/components/App.js
+++ e_motion/frontend/src/components/App.js
@@ -7,6 +7,8 @@ import TrainingResultsPage from "./TrainingResults";
 import MakePredictionsPage from "./MakePredictions";
 import UploadDataset from "./UploadDataset";
 import LabelStudioFrontend from "./LabelStudioFrontend";
+import { SnackbarProvider } from "notistack";
+import Slide from "@mui/material/Slide";
 
 import {
     BrowserRouter as Router,
@@ -15,26 +17,47 @@ import {
     Link,
 } from "react-router-dom";
 
+function SlideTransition(props) {
+    return <Slide {...props} direction="up" />;
+}
+
 
 export default function App(props) {
     return (
-        <div className="center">
-            <Router>
-                <Routes>
-                    <Route
-                        exact path="/"
-                        element={<SignInSide />}
-                    />
-                    <Route path="/login" element={<SignInSide />} />
-                    <Route path="/signup" element={<SignUpSide />} />
-                    <Route path="/dashboard" element={<Dashboard />} />
-                    <Route path="/upload-dataset" element={<UploadDataset />} />
-                    {/* <Route path="/label_studio" element={<LabelStudioFrontend />} /> */}
-                    <Route path="/results" element={<TrainingResultsPage />} />
-                    <Route path="/predict" element={<MakePredictionsPage />} />
-                </Routes>
-            </Router>
-        </div>
+        <SnackbarProvider maxSnack={3} 
+                          TransitionComponent={SlideTransition}
+                          sx={{
+                            '& .MuiSnackbarContent-root': {
+                                '&.MuiSnackbarContent-message': {
+                                    border: '1px solid #4caf50',
+                                },
+                                '&.MuiSnackbarContent-message.Mui-error': {
+                                    border: '1px solid #f44336',
+                                },
+                                '&.MuiSnackbarContent-message.Mui-info': {
+                                    border: '1px solid #2196f3',
+                                },
+                            },
+                        }}
+        >
+            <div className="center">
+                <Router>
+                    <Routes>
+                        <Route
+                            exact path="/"
+                            element={<SignInSide />}
+                        />
+                        <Route path="/login" element={<SignInSide />} />
+                        <Route path="/signup" element={<SignUpSide />} />
+                        <Route path="/dashboard" element={<Dashboard />} />
+                        <Route path="/upload-dataset" element={<UploadDataset />} />
+                        {/* <Route path="/label_studio" element={<LabelStudioFrontend />} /> */}
+                        <Route path="/results" element={<TrainingResultsPage />} />
+                        <Route path="/predict" element={<MakePredictionsPage />} />
+                    </Routes>
+                </Router>
+            </div>
+        </SnackbarProvider>
     );
 }
 

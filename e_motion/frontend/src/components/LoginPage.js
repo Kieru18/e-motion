f8ec60edb6e9efa8bfd4128903735b477ea8830c
kziarek <01169264@pw.edu.pl>
2024-01-16 00:35:56 +0100

    Removed remember me checkbox on login page

--- e_motion/frontend/src/components/LoginPage.js
+++ e_motion/frontend/src/components/LoginPage.js
@@ -3,8 +3,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
@@ -133,10 +131,6 @@ export default function SignInSide() {
             autoComplete="current-password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
           />
           <Button
             type="submit"
